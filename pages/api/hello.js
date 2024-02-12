7346618350ad60e8e0e562de0a2ff6296c8c3d2f
Spartan273 <sami273@live.ca>
2024-02-12 12:15:15 -0500

    1st commit

---
+++ pages/api/hello.js
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+//auth OAuth2
+export default function handler(req, res) {
+  res.status(200).json({ name: 'test 1' })
+}
